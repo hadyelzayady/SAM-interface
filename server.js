ce026e926fee2f701a7c8557cf5c8e8a817bbd96
hadyelzayady <hadyelzayady@yahoo.com>
2019-05-15 15:19:17 +0200

    sadlkjf

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
-// app.get('/*', function (req, res) {
-
-//     res.sendFile(path.join(__dirname + './dist//index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
