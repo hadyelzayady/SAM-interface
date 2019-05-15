0128fcdcbf968d3fb1f6ffd09b94fb786532efe1
hadyelzayady <hadyelzayady@yahoo.com>
2019-05-15 16:44:11 +0200

    adsf

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('./dist/SAM-INTERFACE'))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/SAM-INTERFACE/index.html'));
 });
+
+// 
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
